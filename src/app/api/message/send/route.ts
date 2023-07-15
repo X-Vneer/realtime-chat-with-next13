fe7ee1fdc5867d0c180363a8a83d9316f1935b65
X_V.neer <xv.neer@gmail.com>
2023-07-15 19:31:35 +0300

    trying to fix pusher js

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -1,78 +1,78 @@
-import { generatePusherKey } from "@/src/helpers/utils";
-import { authOptions } from "@/src/lib/auth";
-import { db } from "@/src/lib/db";
-import { pusherSever } from "@/src/lib/pusher";
-import { nanoid } from "nanoid";
-import { getServerSession } from "next-auth";
+import { generatePusherKey } from "@/src/helpers/utils"
+import { authOptions } from "@/src/lib/auth"
+import { db } from "@/src/lib/db"
+import { pusherSever } from "@/src/lib/pusher"
+import { nanoid } from "nanoid"
+import { getServerSession } from "next-auth"
 
 export async function POST(req: Request) {
   try {
     // getting req body
     const { text, chatId } = (await req.json()) as {
-      text: string;
-      chatId: string;
-    };
+      text: string
+      chatId: string
+    }
     if (!text || !chatId)
-      return new Response("invalid body params", { status: 422 });
+      return new Response("invalid body params", { status: 422 })
 
     // getting user session
-    const session = await getServerSession(authOptions);
+    const session = await getServerSession(authOptions)
 
     if (!session || !chatId.includes(session.user.id))
-      return new Response("Unauthorized", { status: 401 });
+      return new Response("Unauthorized", { status: 401 })
 
     // extacting ids and verify both users are friends
-    const [firstUserId, secondUserId] = chatId.split("--");
+    const [firstUserId, secondUserId] = chatId.split("--")
     if (!firstUserId || !secondUserId)
-      return new Response("invalid chatId", { status: 422 });
+      return new Response("invalid chatId", { status: 422 })
 
     const areFriends = await db.sismember(
       `user:${firstUserId}:friends`,
       secondUserId
-    );
+    )
 
-    if (!areFriends) return new Response("Unauthorized", { status: 401 });
+    if (!areFriends) return new Response("Unauthorized", { status: 401 })
 
     // partner data
     const partnerId = chatId.startsWith(session.user.id)
       ? secondUserId
-      : firstUserId;
+      : firstUserId
     // const partner = (await
 
-    const timestamp = Date.now();
+    const timestamp = Date.now()
     const message: Message = {
       id: nanoid(),
       senderId: session.user.id,
       text,
       receiverId: partnerId,
       timestamp,
-    };
+    }
 
     pusherSever.trigger(
       generatePusherKey(`chat:${chatId}`),
       "incomming_message",
       message
-    );
+    )
     pusherSever.trigger(
       generatePusherKey(`user:${partnerId}:chat`),
       "new_unseen_message",
       message
-    );
+    )
 
-    const [partner] = await Promise.all([
+    await Promise.all([
       db.get<User>(`user:${partnerId}`),
       db.zadd(`chat:${chatId}:messages`, {
         score: timestamp,
         member: message,
       }),
-    ]);
+    ])
 
-    return new Response("ok");
+    return new Response("ok")
   } catch (error) {
     if (error instanceof Error) {
-      return new Response(error.message, { status: 500 });
+      return new Response(error.message, { status: 500 })
     }
 
-    return new Response("Internal server error", { status: 500 });
+    return new Response("Internal server error", { status: 500 })
   }
 }
